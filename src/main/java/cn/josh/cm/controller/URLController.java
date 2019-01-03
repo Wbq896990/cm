2f324f8469cb0c1f5e707cf4baf0140eaa17eefc
lixy11 <15201495150@163.com>
2019-01-03 20:40:50 +0800

    添加spring 依赖

--- src/main/java/cn/josh/cm/controller/URLController.java
+++ src/main/java/cn/josh/cm/controller/URLController.java
@@ -2,6 +2,7 @@ package cn.josh.cm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Author: Josh.Wang
@@ -10,14 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Modified By:
  */
 @Controller
+@RequestMapping("/cm")
 public class URLController {
 
     @RequestMapping(value = "/success")
+    @ResponseBody
     public String success()throws Exception{
         return "/success";
     }
 
     @RequestMapping(value = "/index")
+    @ResponseBody
     public String index()throws Exception{
         return "/index";
     }
